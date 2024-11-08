d327af71e822fc19f0eef50a7186e914c86a56b2
Gabriel (Egor Guschin) Lutskii <115532859+GBLutskii@users.noreply.github.com>
2024-11-08 17:11:53 +0300

    solution

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codewars_Snail_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] array = { new []{1, 2, 3}, new []{4, 5, 6}, new []{7, 8, 9} };
+            foreach (int i in SnailSolution.Snail(array)) { Console.Write(i); };
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Snail_array_class.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class SnailSolution
+{
+    public static int[] Snail(int[][] array)
+    {
+        List<int> result = new List<int>();
+
+        if (array[0].Length == 0)
+        {
+            return array[0];
+        }
+
+        int len = array.GetLength(0) - 1;
+        int x, y, count;
+        count = 0;
+        int border = array.GetLength(0) / 2;
+
+        while (count < border)
+        {
+            x = y = count;
+
+            while (x != (len - count))
+            {
+                if (y >= count && y < (len - count))
+                {
+                    result.Add(array[x][y]);
+                    y++;
+                }
+                else
+                {
+                    result.Add(array[x][y]);
+                    x++;
+                }
+            }
+
+            while (x != count)
+            {
+                if (y > count && y <= (len - count))
+                {
+                    result.Add(array[x][y]);
+                    y--;
+                }
+                else
+                {
+                    result.Add(array[x][y]);
+                    x--;
+                }
+            }
+
+            count++;
+        }
+
+        if (array.GetLength(0) % 2 != 0)
+        {
+            result.Add(array[border][border]);
+        }
+
+        return result.ToArray();
+    }
+}
